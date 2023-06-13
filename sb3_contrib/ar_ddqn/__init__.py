e8e64e0ec27b590f8839e23eb70b53d9dc7c4b54
Clément Dumas <dumasclement2002@gmail.com>
2023-06-13 11:08:24 +0200

    copy/paste sb3 DQN

---
+++ sb3_contrib/ar_ddqn/__init__.py
@@ -0,0 +1,5 @@
+from sb3_contrib.ar_ddqn.ar_ddqn import DQN
+from sb3_contrib.ar_ddqn.policies import CnnPolicy, MlpPolicy, MultiInputPolicy
+
+__all__ = ["CnnPolicy", "MlpPolicy", "MultiInputPolicy", "DQN"]
+# todo: figure out this __all__
