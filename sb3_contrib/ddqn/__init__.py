dbb04d95d8f7544823c2e37d31de5a3e3a453839
Clément Dumas <dumasclement2002@gmail.com>
2023-06-21 14:53:21 +0200

    remove ddqn

--- sb3_contrib/ddqn/__init__.py
+++
@@ -1,4 +0,0 @@
-from sb3_contrib.ddqn.ddqn import DDQN
-from sb3_contrib.ddqn.policies import CnnPolicy, MlpPolicy, MultiInputPolicy
-
-__all__ = ["CnnPolicy", "MlpPolicy", "MultiInputPolicy", "DDQN"]
