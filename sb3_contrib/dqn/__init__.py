8d15ffa10ca57df8e2d87c6c428d3152e7d2ad67
Clément Dumas <dumasclement2002@gmail.com>
2023-06-22 23:55:16 +0200

    added a custom DQN to have costum logs

---
+++ sb3_contrib/dqn/__init__.py
@@ -0,0 +1,4 @@
+from sb3_contrib.dqn.dqn import DQN
+from stable_baselines3.dqn.policies import CnnPolicy, MlpPolicy, MultiInputPolicy
+
+__all__ = ["CnnPolicy", "MlpPolicy", "MultiInputPolicy", "DQN"]
