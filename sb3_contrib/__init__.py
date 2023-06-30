a1ec5f5ab08ff2aedd14b499087808440f094511
Clément Dumas <dumasclement2002@gmail.com>
2023-06-30 18:42:27 +0200

    added q learning

--- sb3_contrib/__init__.py
+++ sb3_contrib/__init__.py
@@ -4,6 +4,7 @@ from sb3_contrib.ar_dqn import ArDQN
 from sb3_contrib.ars import ARS
 from sb3_contrib.ppo_mask import MaskablePPO
 from sb3_contrib.ppo_recurrent import RecurrentPPO
+from sb3_contrib.q_learning import QLearning
 from sb3_contrib.qrdqn import QRDQN
 from sb3_contrib.tqc import TQC
 from sb3_contrib.trpo import TRPO
@@ -13,12 +14,4 @@ version_file = os.path.join(os.path.dirname(__file__), "version.txt")
 with open(version_file) as file_handler:
     __version__ = file_handler.read().strip()
 
-__all__ = [
-    "ARS",
-    "MaskablePPO",
-    "RecurrentPPO",
-    "QRDQN",
-    "TQC",
-    "TRPO",
-    "ArDQN",
-]
+__all__ = ["ARS", "MaskablePPO", "RecurrentPPO", "QRDQN", "TQC", "TRPO", "ArDQN", "QLearning"]
---
+++ sb3_contrib/q_learning/__init__.py
@@ -0,0 +1,4 @@
+from sb3_contrib.q_learning.policies import TabularPolicy
+from sb3_contrib.q_learning.q_learning import QLearning
+
+__all__ = ["TabularPolicy", "QLearning"]
