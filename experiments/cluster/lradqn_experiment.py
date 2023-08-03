30ef67f8b750e0074c592dfae13a2b15eb5a2a03
Clement <clement.dumas@ens-paris-saclay.fr>
2023-08-03 02:36:40 +0200

    hotfix

--- experiments/cluster/lradqn_experiment.py
+++ experiments/cluster/lradqn_experiment.py
@@ -7,15 +7,15 @@ from slurm import submit_job_array
 from itertools import product
 import time
 
-NB_LRA = 30
-MODEL_PER_LRA = 10
-EXPERIMENT_NAME = "lra_experiment"
+NB_LRA = 3
+MODEL_PER_LRA = 1
+EXPERIMENT_NAME = "test_lra_experiment"
 ENV_ID = EMPTY_GRID
 LOG_PATH = path.join("logs", EXPERIMENT_NAME, ENV_ID)
 LOCAL_RELATIVE_ASPIRATIONS = list(np.linspace(0, 1, NB_LRA))
 TRAIN_DQN = True
-N_EVAL_EPISODES = 100
-LEARNING_STEPS = 1_000_000
+N_EVAL_EPISODES = 1
+LEARNING_STEPS = 10_000
 
 if __name__ == "__main__":
     # Compute the cartesian product of rhos mus and aspirations
@@ -34,14 +34,15 @@ if __name__ == "__main__":
     post_args = {
         "names": lra_names,
         "log_path": LOG_PATH,
+	"expected_time_steps": LEARNING_STEPS
     }
     submit_job_array(
-        "lra_worker.py",
+        "lradqn_worker.py",
         worker_args,
         nb_exp,
         EXPERIMENT_NAME + "_" + ENV_ID,
-        post_python_file="lra_post_experiment.py",
+        post_python_file="lradqn_post_experiment.py",
         post_args=post_args,
         testing=False,
-        wandb_sync=True,
+        wandb_sync=False,
     )
