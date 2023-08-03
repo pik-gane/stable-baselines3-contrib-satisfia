281fc4937a66c092a697cb66e1ca9adafe43dce1
Clément Dumas <dumasclement2002@gmail.com>
2023-08-03 03:06:18 +0200

    revert to normal experiment

--- experiments/cluster/lradqn_experiment.py
+++ experiments/cluster/lradqn_experiment.py
@@ -7,15 +7,15 @@ from slurm import submit_job_array
 from itertools import product
 import time
 
-NB_LRA = 3
-MODEL_PER_LRA = 1
+NB_LRA = 20
+MODEL_PER_LRA = 10
 EXPERIMENT_NAME = "test_lra_experiment"
 ENV_ID = EMPTY_GRID
 LOG_PATH = path.join("logs", EXPERIMENT_NAME, ENV_ID)
 LOCAL_RELATIVE_ASPIRATIONS = list(np.linspace(0, 1, NB_LRA))
 TRAIN_DQN = True
-N_EVAL_EPISODES = 1
-LEARNING_STEPS = 10_000
+N_EVAL_EPISODES = 100
+LEARNING_STEPS = 1_000_000
 
 if __name__ == "__main__":
     # Compute the cartesian product of rhos mus and aspirations
@@ -40,5 +40,5 @@ if __name__ == "__main__":
         post_python_file="lradqn_post_experiment.py",
         post_args=post_args,
         testing=False,
-        wandb_sync=False,
+        wandb_sync=True,
     )
